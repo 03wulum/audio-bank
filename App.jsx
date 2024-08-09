5a3e7a8093b7809e2394f4af3ac25a5a7570cd3c
Obinna Ewulum <obinnaewulum@gmail.com>
2024-08-09 00:12:53 -0400

    Remove vosk, use expo file system for saving audio.

--- App.jsx
+++ App.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+import React, {useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import HomeScreen from './screens/HomeScreen';
+import TranscriptionsScreen from './screens/TranscriptionsScreen';
 
 const Drawer = createDrawerNavigator();
 
 const App = () => {
+  const [transcriptions, setTranscriptions] = useState([]);
+
+  const handleTranscriptionComplete = newTranscription => {
+    setTranscriptions([...transcriptions, newTranscription]);
+  };
+
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        {/* Add more screens here */}
+        <Drawer.Screen name="Home">
+          {props => (
+            <HomeScreen
+              {...props}
+              onTranscriptionComplete={handleTranscriptionComplete}
+            />
+          )}
+        </Drawer.Screen>
+        <Drawer.Screen name="Transcriptions">
+          {props => (
+            <TranscriptionsScreen {...props} transcriptions={transcriptions} />
+          )}
+        </Drawer.Screen>
       </Drawer.Navigator>
     </NavigationContainer>
   );
--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 This is a new [**React Native**](https://reactnative.dev) project, bootstrapped using [`@react-native-community/cli`](https://github.com/react-native-community/cli).
 
+# Quick Start
+
+run the app with
+
+```
+ npx expo start --clear
+```
+
+the use the expo app to scan the qr code in the terminal. 
+
 # Getting Started
 
 >**Note**: Make sure you have completed the [React Native - Environment Setup](https://reactnative.dev/docs/environment-setup) instructions till "Creating a new application" step, before proceeding.
@@ -11,9 +21,6 @@ First, you will need to start **Metro**, the JavaScript _bundler_ that ships _wi
 To start Metro, run the following command from the _root_ of your React Native project:
 
 ```bash
-# using npm
-npm start
-
 # OR using Yarn
 yarn start
 ```
